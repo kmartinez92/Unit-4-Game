cb45cbfeccc8e4aabe0fde7e13c37d01cde3acfb
Kelvin <kelvin.martinez92@gmail.com>
2019-12-11 21:21:22 -0500

    updates to game

---
+++ assets/javascript/game.js
@@ -0,0 +1,9 @@
+var randomNumber = 0; 
+var buttonNumber = 0; 
+var sumOfNumbers = 0;
+
+
+var random = Math.floor((Math.random() * 120) + 1); 
+    document.write("Computer Number Is " + random ); 
+
+
